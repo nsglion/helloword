ce444fa9b87cad274e6fd60527e9f3f4da1e94e8
pbaier01 <pbaier01@TOOLS12.dcs.bbk.ac.uk>
2014-01-20 20:24:52 +0000

    helloword

---
+++ helloworld/src/helloworld/helloworld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class helloworld 
+{
+	public static void main(String[] args)
+	{
+		int i;
+		int x = 0;
+		for (i=1; i<=10; i++)
+		{	
+			x = x + i;
+			
+		}
+		System.out.println(x); 
+	}
+
+}
---
+++ helloworld/src/helloworld/input.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class input 
+{
+	public static void main(String[] args)
+	{
+	
+	Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		int y = in.nextInt();
+		System.out.println(x+y);
+	}
+}
